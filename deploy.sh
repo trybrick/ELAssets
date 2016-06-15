db117bd1499cf8cd926758959393210d3e0f8fa8
noogen <tom@noogen.net>
2016-06-14 21:03:45 -0500

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 DEST_BRANCH_DEFAULT=master
 DEST_BRANCH=${1:-$DEST_BRANCH_DEFAULT}
 
-aws s3 sync "./asset/bootstrap" "s3://brick-web/ds/$DEST_BRANCH/asset/bootstrap"
+aws s3 sync "./src" "s3://brick-web/ds/$DEST_BRANCH/assets"
